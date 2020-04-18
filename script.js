48fbbab256e16f368b630e452c835f1351b8ef68
Michał Czaboryk <m.czaboryk@gmail.com>
2020-04-18 18:47:14 +0200

    JS add gameplay and winner styling

--- script.js
+++ script.js
@@ -17,22 +17,50 @@ function TicTacToeGame() {
     }
 
     function takeTurn() {
-        // console.log("It works");
+        if (board.checkForWinner()) {
+            return;
+        }
 
         if (turn % 2 === 0) {
             player1.takeTurn();
         } else {
             player2.takeTurn();
         }
-
         turn++;
     }
 }
 
 function Board() {
-    this.positions = Array.from(document.querySelectorAll('.col'));
-    console.log(this.positions);
 
+    this.positions = Array.from(document.querySelectorAll('.col'));
+    this.checkForWinner = function () {
+        let winner = false;
+        const winningCombinations = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        const positions = this.positions;
+        winningCombinations.forEach((winningCombo) => {
+            const pos0InnerText = positions[winningCombo[0]].innerText;
+            const pos1InnerText = positions[winningCombo[1]].innerText;
+            const pos2InnerText = positions[winningCombo[2]].innerText;
+            const isWinningCombo = pos0InnerText !== '' &&
+                pos0InnerText === pos1InnerText && pos1InnerText === pos2InnerText;
+            if (isWinningCombo) {
+                winner = true;
+                winningCombo.forEach((index) => {
+                    positions[index].className += ' winner';
+                })
+            }
+        });
+        return winner;
+    }
 }
 
 function Player1(board) {
@@ -50,8 +78,8 @@ function Player1(board) {
 
 function Player2(board) {
     this.takeTurn = function () {
-        const availablePositions = board.positions.filter((p) => p.innerText === '');
-        const move = Math.floor(Math.random() * availablePositions.length);
+        let availablePositions = board.positions.filter((p) => p.innerText === '');
+        const move = Math.floor(Math.random() * (availablePositions.length - 0));
         availablePositions[move].innerText = 'O';
         console.log("Player2 turn");
     }
